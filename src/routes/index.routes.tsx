2b6f3167455a6a628ce005355a8a04ff64deefe8
Coraliedev <coralie.acquaert@gmail.com>
2022-08-24 09:19:30 +0200

    feat(routes): edit routes to redirect to the outliner page

--- src/routes/index.routes.tsx
+++ src/routes/index.routes.tsx
@@ -1,19 +1,25 @@
 import React from "react";
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import {
+  BrowserRouter as Router,
+  Navigate,
+  Route,
+  Routes,
+} from "react-router-dom";
 import Editor from "../pages/editor.page";
 import Outliner from "../pages/outliner.page";
 
 const index = () => {
-    return (
-        <div>
-            <Router>
-                <Routes>
-                    <Route path="/editor" element={<Editor/>} />
-                    <Route path="/outliner" element={<Outliner/>} />
-                </Routes>
-            </Router>
-        </div>
-    )
-}
+  return (
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/editor" element={<Editor />} />
+          <Route path="/outliner" element={<Outliner />} />
+          <Route path="/" element={<Navigate to="/outliner" />} />
+        </Routes>
+      </Router>
+    </div>
+  );
+};
 
 export default index;
