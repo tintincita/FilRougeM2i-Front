51f763d13c9d9f0d379600cfc589b21592c276f0
Coraliedev <coralie.acquaert@gmail.com>
2022-08-24 09:10:28 +0200

    fix: change document model

--- src/models/document.model.tsx
+++ src/models/document.model.tsx
@@ -1,14 +1,17 @@
 export default class DocumentModel {
   title: string;
-  cards: [string];
+  cards: string[];
   parentSpace: string;
+  id:string;
   constructor(
     title: string = "",
-    cards: [string] = [""],
-    parentSpace: string = ""
+    cards: string[] = [""],
+    parentSpace: string = "",
+    id:string = ""
   ) {
     this.title = title;
     this.cards = cards;
     this.parentSpace = parentSpace;
+    this.id = id;
   }
 }
