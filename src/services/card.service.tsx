7fbe5cc72fdad59be8cf0f04e29e2b0e2c193447
Coraliedev <coralie.acquaert@gmail.com>
2022-08-24 18:32:00 +0200

    docs: docs of services card

--- src/services/card.service.tsx
+++ src/services/card.service.tsx
@@ -8,7 +8,13 @@ import { AppDispatch } from "../store/store";
 import { deleteCardById } from "../features/cards/cardsSlice";
 import API from "../config/config.json"
 
-export function fetchAllCards() {
+
+/**
+ * This function is an async function that takes a dispatch function as an argument and returns a
+ * function that makes an API call and dispatches the response to the reducer.
+ * @returns {Function} - A function that makes an API call and dispatches the response to the reducer.
+ */
+export function fetchAllCards(): (dispatch: AppDispatch) => Promise<void> {
   return async (dispatch: AppDispatch) => {
     try {
       const response = await axios
@@ -21,7 +27,14 @@ export function fetchAllCards() {
   };
 }
 
-export function deleteCard(id: string) {
+/**
+ * It's an async function that takes an id as a parameter, and returns a function that takes a dispatch
+ * function as a parameter, and returns a promise that deletes a card from the database, and then
+ * dispatches an action to delete the card from the redux store.
+ * @param {string} id - string
+ * @returns The return type is a function that takes a dispatch function as an argument.
+ */
+export function deleteCard(id: string): (dispatch: AppDispatch) => Promise<void> {
   return async (dispatch: AppDispatch) => {
     try {
       await axios.delete(`${API.api.deleteCardByID}${id}`).then(() => {
@@ -34,7 +47,15 @@ export function deleteCard(id: string) {
   };
 }
 
-export function fetchEditorCardsByIdDocument(ids: string) {
+/**
+ * It's a function that takes an id as a parameter, and returns a function that takes a dispatch
+ * function as a parameter, and returns a promise that dispatches a function that takes the response as
+ * a parameter.
+ * @param {string} ids - string
+ * @returns The return type is a function that takes a dispatch function as an argument.
+ * 
+ */
+export function fetchEditorCardsByIdDocument(ids: string): (dispatch: AppDispatch) => Promise<void> {
   return async (dispatch: AppDispatch) => {
     try {
       const response = await axios
@@ -48,7 +69,14 @@ export function fetchEditorCardsByIdDocument(ids: string) {
   };
 }
 
-export function fetchOutlinerCardsByIdDocument(ids: string) {
+/**
+ * It's a function that takes a string as an argument, and returns a function that takes a dispatch
+ * function as an argument, and returns a promise that takes a response as an argument, and returns a
+ * dispatch function that takes a response as an argument, and returns a console.log of the response.
+ * @param {string} ids - string
+ * @returns The return type is a function that takes a dispatch function as an argument.
+ */
+export function fetchOutlinerCardsByIdDocument(ids: string): (dispatch: AppDispatch) => Promise<void> {
   return async (dispatch: AppDispatch) => {
     try {
       const response = await axios
@@ -62,7 +90,13 @@ export function fetchOutlinerCardsByIdDocument(ids: string) {
   };
 }
 
-export function NewCard(ids: string) {
+/**
+ * It takes a string as an argument, makes a post request to an API, and then dispatches an action to
+ * the reducer.
+ * @param {string} ids - string
+ * @returns a function that returns a promise.
+ */
+export function NewCard(ids: string): (dispatch: AppDispatch) => Promise<void> {
   return async (dispatch: AppDispatch) => {
     try {
       await axios({
