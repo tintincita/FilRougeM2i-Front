7d4ed0984dfc97d92f308dcc310dc42f810cae0b
merlink <merlincita@gmail.com>
2022-09-21 15:59:23 +0200

    tests: setup

---
+++ src/__tests__/header_component.test.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { render } from "@testing-library/react";
+
+import {Header} from "../components/header/header.component";
+
+describe("<Header />", () => {
+  test("should display app name", async () => {
+    // ???
+  });
+});
