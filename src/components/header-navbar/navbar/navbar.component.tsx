e6893297afee38ce39dbb72be86763c40e6056f2
Coraliedev <coralie.acquaert@gmail.com>
2022-10-07 10:49:47 +0200

    style: responsive Outliner Page

--- src/components/header-navbar/navbar/navbar.component.tsx
+++ src/components/header-navbar/navbar/navbar.component.tsx
@@ -10,11 +10,9 @@ export const NavBar: React.FC<NavProps> = ({ className }) => {
     <nav className={className}>
       <a className={className + "_outliner"} href="/outliner">
         <TbBoxMultiple className="icon_outliner" />
-        <span>Outliner</span>
       </a>
       <a className={className + "_editor"} href="/editor">
         <TbFile className="icon_editor" />
-        <span>Editor</span>
       </a>
     </nav>
   );
