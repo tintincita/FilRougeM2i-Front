ae22702363eb28177554558c18fceb5fe6bd24b1
Coraliedev <coralie.acquaert@gmail.com>
2022-10-07 12:25:47 +0200

    style: home page

--- src/pages/home/home.page.tsx
+++ src/pages/home/home.page.tsx
@@ -1,9 +1,11 @@
 import { Header } from "../../components/header-navbar/header/header.component";
+import "./home.page.css";
 
 const HomePage = () => {
   return (
     <div className="homePage">
       <Header className="homePage_nav" />
+      <div className="home"></div>
     </div>
   );
 };
