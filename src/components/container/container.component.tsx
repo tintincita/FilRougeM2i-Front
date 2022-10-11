7659754b716bc26c9e5cf42313d47989baaed386
Coraliedev <coralie.acquaert@gmail.com>
2022-10-11 11:17:00 +0200

    feat(container): create button allow or not the title editing

--- src/components/container/container.component.tsx
+++ src/components/container/container.component.tsx
@@ -2,37 +2,50 @@ import { useMutation, useQueryClient } from "react-query";
 import { updateTitleDocumentById } from "../../services/document.service";
 import { updateProjectTitleById } from "../../services/project.service";
 import { updateTitleWorkspaceById } from "../../services/workspace.service";
-import {GrFormNextLink} from "react-icons/gr";
+import { GrFormNextLink } from "react-icons/gr";
 import "./container.css";
+import { FiEdit } from "react-icons/fi";
 
+let edit = false;
 interface WorkspaceProps {
   entity: any;
 }
 export const Container: React.FC<WorkspaceProps> = ({ entity }) => {
   const queryClient = useQueryClient();
 
+/* A react-query hook that is used to update the title of a workspace. */
   const { mutate: updateTitleW } = useMutation(updateTitleWorkspaceById, {
     onSuccess: () => {
       queryClient.invalidateQueries("workspaces");
     },
   });
+
+/* A react-query hook that is used to update the title of a project. */
   const { mutate: updateTitleP } = useMutation(updateProjectTitleById, {
     onSuccess: () => {
       queryClient.invalidateQueries("workspaces");
     },
   });
 
+/* A react-query hook that is used to update the title of a document. */
   const { mutate: updateTitleD } = useMutation(updateTitleDocumentById, {
     onSuccess: () => {
       queryClient.invalidateQueries("workspaces");
     },
   });
 
+/* Creating an object that will be used to update the title of the entity. */
   let update = {
     Id: entity._id,
     title: entity.title,
   };
 
+ /**
+  * When the user types in the input field, the value of the input field is assigned to the title
+  * property of the update object, and then the update object is passed to the appropriate function to
+  * update the title of the entity.
+  * @param {any} e - any - the event object
+  */
   function updateTitleEntity(e: any) {
     e.preventDefault();
     e.stopPropagation();
@@ -48,6 +61,11 @@ export const Container: React.FC<WorkspaceProps> = ({ entity }) => {
     }
   }
 
+  /**
+   * If the entity has a property called projects, documents, or outlinerCards, then go to the next
+   * page.
+   * @param {any} e - any - the event that is passed to the function
+   */
   function goToNextPage(e: any) {
     e.preventDefault();
     e.stopPropagation();
@@ -62,14 +80,36 @@ export const Container: React.FC<WorkspaceProps> = ({ entity }) => {
     }
   }
 
+/**
+ * If the edit variable is true, set it to false. If it's false, set it to true. Then, invalidate the
+ * queries for workspaces, projects, and documents.
+ * @param {any} e - any - this is the event that is passed to the function.
+ */
+  function editTitle(e: any) {
+    if (edit) {
+      edit = false;
+    } else {
+      edit = true;
+    }
+    queryClient.invalidateQueries("workspaces");
+    queryClient.invalidateQueries("projects");
+    queryClient.invalidateQueries("documents");
+  }
+
   return (
     <div className="container">
-      <h2>{entity.title}</h2>
-      <input
-        type="text"
-        defaultValue={entity.title}
-        onChange={updateTitleEntity}
-      ></input>
+      {edit ? (
+        <input
+          type="text"
+          defaultValue={entity.title}
+          onChange={updateTitleEntity}
+        ></input>
+      ) : (
+        <h2>{entity.title}</h2>
+      )}
+      <button onClick={editTitle} className="edit">
+        <FiEdit />
+      </button>
       <button onClick={goToNextPage} className="next">
         <GrFormNextLink />
       </button>
