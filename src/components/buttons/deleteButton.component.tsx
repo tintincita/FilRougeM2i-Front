a8aeb603df2e1769a5994025e39a54a2e9a2e470
Coraliedev <coralie.acquaert@gmail.com>
2022-10-15 15:56:43 +0200

    fix: toolbar style buttons

--- src/components/buttons/deleteButton.component.tsx
+++ src/components/buttons/deleteButton.component.tsx
@@ -1,30 +1,48 @@
+import { useEffect } from "react";
 import { AiOutlineDelete } from "react-icons/ai";
 import "./buttons.css";
 
 export const DeleteButton: React.FC = () => {
-  let deleteButton =document.getElementsByClassName("delete_card")[0] as HTMLButtonElement;
-  let editButton =document.getElementsByClassName("edit_card")[0] as HTMLButtonElement;
+  // useEffect(() => {
+  //   sessionStorage.setItem("DeleteButton", "disabled");
+  // }, []);
+
+
   /**
    * If the delete button is enabled, disable it. If the delete button is disabled, enable it. If the
    * edit button is enabled, disable it and enable the delete button.
    * @param {any} e - any - the event that is triggered when the button is clicked
    */
   function deleteButtonOnClick(e: any) {
+    let deleteButton = document.getElementsByClassName(
+      "delete_card"
+    )[0] as HTMLButtonElement;
+    let editButton = document.getElementsByClassName(
+      "edit_card"
+    )[0] as HTMLButtonElement;
     if (sessionStorage.getItem("DeleteButton") === "enabled") {
       sessionStorage.setItem("DeleteButton", "disabled");
-      deleteButton.style.color="#131313";
+      if (deleteButton) {
+        deleteButton.style.color = "#131313";
+      }
     } else {
       sessionStorage.setItem("DeleteButton", "enabled");
-      deleteButton.style.color="rgba(123, 194, 252, 0.75)";
+      if (deleteButton) {
+        deleteButton.style.color = "rgba(123, 194, 252, 0.75)";
+      }
     }
     if (sessionStorage.getItem("EditButton") === "enabled") {
       sessionStorage.setItem("EditButton", "disabled");
-      editButton.style.color="#131313";
+      if (editButton) {
+        editButton.style.color = "#131313";
+      }
       sessionStorage.setItem("DeleteButton", "enabled");
-      deleteButton.style.color="rgba(123, 194, 252, 0.75)";
+      if (deleteButton) {
+        deleteButton.style.color = "rgba(123, 194, 252, 0.75)";
+      }
     }
   }
-  
+
   return (
     <button className="delete_card" onClick={deleteButtonOnClick}>
       <AiOutlineDelete />
--- src/components/buttons/editButton.component.tsx
+++ src/components/buttons/editButton.component.tsx
@@ -1,15 +1,16 @@
+import { useEffect } from "react";
 import { FiEdit } from "react-icons/fi";
 import { useQueryClient } from "react-query";
 import "./buttons.css";
 
 export const EditButton: React.FC = () => {
   const queryClient = useQueryClient();
-  let editButton = document.getElementsByClassName(
-    "edit_card"
-  )[0] as HTMLButtonElement;
-  let deleteButton = document.getElementsByClassName(
-    "delete_card"
-  )[0] as HTMLButtonElement;
+
+  // useEffect(() => {
+  //   sessionStorage.setItem("EditButton", "disabled");
+  // }, []);
+
+
   /**
    *  If the edit button is enabled, disable it. If the edit button is disabled, enable it. If the
    * delete button is enabled, disable it and enable the edit button.
@@ -21,15 +22,30 @@ export const EditButton: React.FC = () => {
   }) {
     e.preventDefault();
     e.stopPropagation();
+
+    let editButton = document.getElementsByClassName(
+      "edit_card"
+    )[0] as HTMLButtonElement;
+    let deleteButton = document.getElementsByClassName(
+      "delete_card"
+    )[0] as HTMLButtonElement;
+  
+    console.log("editButton", editButton);
     if (sessionStorage.getItem("EditButton") === "enabled") {
       sessionStorage.setItem("EditButton", "disabled");
-      editButton.style.color = "#131313";
+      if (editButton) {
+        editButton.style.color = "#131313";
+      }
     } else {
       sessionStorage.setItem("EditButton", "enabled");
-      editButton.style.color = "rgba(123, 194, 252, 0.75)";
+      if (editButton) {
+        editButton.style.color = "rgba(123, 194, 252, 0.75)";
+      }
 
       sessionStorage.setItem("DeleteButton", "disabled");
-      deleteButton.style.color = "#131313";
+      if (deleteButton) {
+        deleteButton.style.color = "#131313";
+      }
     }
     queryClient.invalidateQueries("outlinerCards");
     queryClient.invalidateQueries("editorCards");
