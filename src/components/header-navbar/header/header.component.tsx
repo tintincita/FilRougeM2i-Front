08b1064d09c7d233af34ae22b9293566625a0c2b
Coraliedev <coralie.acquaert@gmail.com>
2022-10-16 17:14:37 +0200

    style: fix style of application

--- src/components/header-navbar/header/header.component.tsx
+++ src/components/header-navbar/header/header.component.tsx
@@ -1,7 +1,5 @@
 import { NavBar } from "../navbar/navbar.component";
-import { MdLogin, MdLogout } from "react-icons/md";
 import "./header.css";
-import { Link } from "react-router-dom";
 const logo = require("./logo.png");
 
 interface HeaderProps {
@@ -17,15 +15,6 @@ export const Header: React.FC<HeaderProps> = ({ className, id }) => {
       {className === "editor_nav" || className === "outliner_nav" ? (
         <NavBar className={className} id={id} />
       ) : null}
-      {className === "homePage_nav" ? (
-        <Link to="/workspace">
-          <MdLogin className="login_icon" id="login_icon" />
-        </Link>
-      ) : (
-        <Link to="/homepage">
-          <MdLogout className="logout_icon" id="logout_icon" />
-        </Link>
-      )}
     </div>
   );
 };
--- src/routes/index.routes.tsx
+++ src/routes/index.routes.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import {
   BrowserRouter as Router,
   Navigate,
@@ -7,7 +6,6 @@ import {
 } from "react-router-dom";
 import { DocumentPage } from "../pages/document/document.page";
 import Editor from "../pages/editor/editor.page";
-import HomePage from "../pages/home/home.page";
 import Outliner from "../pages/outliner/outliner.page";
 import { ProjectPage } from "../pages/project/project.page";
 import { WorkspacePage } from "../pages/workspace/workspace.page";
@@ -22,8 +20,7 @@ const index = () => {
           <Route path="/workspace" element={<WorkspacePage />} />
           <Route path="/editor/:id" element={<Editor />} />
           <Route path="/outliner/:id" element={<Outliner />} />
-          <Route path="/homePage" element={<HomePage />} />
-          <Route path="/" element={<Navigate to="/homePage" />} />
+          <Route path="/" element={<Navigate to="/workspace" />} />
         </Routes>
       </Router>
     </div>
