28eaa023037b41762a9c7c756c4e8734fed59417
Coraliedev <coralie.acquaert@gmail.com>
2022-10-16 16:33:34 +0200

    style: style of application

--- src/components/header-navbar/header/header.component.tsx
+++ src/components/header-navbar/header/header.component.tsx
@@ -2,6 +2,7 @@ import { NavBar } from "../navbar/navbar.component";
 import { MdLogin, MdLogout } from "react-icons/md";
 import "./header.css";
 import { Link } from "react-router-dom";
+const logo = require("./logo.png");
 
 interface HeaderProps {
   className?: string;
@@ -11,6 +12,7 @@ interface HeaderProps {
 export const Header: React.FC<HeaderProps> = ({ className, id }) => {
   return (
     <div className="header">
+      <img src={logo} alt="logo" className="header_logo" />
       <h1 className="application_name">Ada</h1>
       {className === "editor_nav" || className === "outliner_nav" ? (
         <NavBar className={className} id={id} />
