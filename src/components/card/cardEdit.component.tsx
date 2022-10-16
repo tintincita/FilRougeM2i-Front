9dafdbdf2a20450af683d0c9bfe5f218afe83c71
Coraliedev <coralie.acquaert@gmail.com>
2022-10-16 13:37:03 +0200

    style: fix style of card selected by id

--- src/components/card/cardEdit.component.tsx
+++ src/components/card/cardEdit.component.tsx
@@ -14,17 +14,16 @@ export const CardEdit: React.FC<CardEditProps> = ({
   id,
   className,
 }) => {
-  
   const queryClient = useQueryClient();
 
-/* Creating an object with the id, title and content for update cards. */
+  /* Creating an object with the id, title and content for update cards. */
   let update = {
     id: id,
     title: title,
     content: content,
   };
 
- /* A hook that is used to update the card. */
+  /* A hook that is used to update the card. */
   const { mutate: updateCard } = useMutation(updateCardById, {
     onSuccess: () => {
       queryClient.invalidateQueries("outlinerCards");
@@ -32,56 +31,52 @@ export const CardEdit: React.FC<CardEditProps> = ({
     },
   });
 
-
-/* A function that is used to resize the textarea when the user is typing. */
-  const textAreaContent= document.getElementsByClassName("selectedCardById_card_content")[0] as HTMLTextAreaElement;
+  /* A function that is used to resize the textarea when the user is typing. */
+  const textAreaContent = document.getElementsByClassName(
+    "selectedCardById_card_content"
+  )[0] as HTMLTextAreaElement;
   if (textAreaContent) {
-  textAreaContent.addEventListener("keyup", (e) => {
-    let scHeight = textAreaContent.scrollHeight;
-    textAreaContent.style.height = "auto";
-    textAreaContent.style.height = scHeight + "px";
-  });}
+    textAreaContent.addEventListener("keyup", (e) => {
+      let scHeight = textAreaContent.scrollHeight;
+      textAreaContent.style.height = "auto";
+      textAreaContent.style.height = scHeight + "px";
+    });
+  }
 
-/**
-  * "onChangeTitle is a function that takes an event as an argument, and then updates the title of
-  * the card with the value of the event."
-  * 
-  * @param e - React.ChangeEvent<HTMLTextAreaElement>
-  */
+  /**
+   * "onChangeTitle is a function that takes an event as an argument, and then updates the title of
+   * the card with the value of the event."
+   *
+   * @param e - React.ChangeEvent<HTMLTextAreaElement>
+   */
   const onChangeTitle = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
     e.preventDefault();
     update.title = e.target.value;
     updateCard(update);
   };
 
- 
- /**
-  * "onChangeContent is a function that takes an event as an argument, and then updates the content of
-  * the card with the value of the event."
-  * 
-  * @param e - React.ChangeEvent<HTMLTextAreaElement>
-  */
+  /**
+   * "onChangeContent is a function that takes an event as an argument, and then updates the content of
+   * the card with the value of the event."
+   *
+   * @param e - React.ChangeEvent<HTMLTextAreaElement>
+   */
   const onChangeContent = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
     e.preventDefault();
     update.content = e.target.value;
     updateCard(update);
   };
 
- /**
-  * It removes the selectedCard from sessionStorage and then invalidates the query.
-  */
+  /**
+   * It removes the selectedCard from sessionStorage and then invalidates the query.
+   */
   function closeSelectedCard() {
     sessionStorage.removeItem("selectedCard");
     queryClient.invalidateQueries("editorCards");
   }
 
   return (
-    <div>
-      {className === "selectedCardById" ? (
-        <div className={className + "_close"}>
-          <BsX onClick={closeSelectedCard} />
-        </div>
-      ) : null}
+    <div className="card_edit">
       <textarea
         id={id}
         name="title"
@@ -90,6 +85,11 @@ export const CardEdit: React.FC<CardEditProps> = ({
         onChange={onChangeTitle}
         spellCheck="false"
       />
+      {className === "selectedCardById" ? (
+        <div className={className + "_close"}>
+          <BsX onClick={closeSelectedCard} />
+        </div>
+      ) : null}
       <textarea
         id={id}
         name="content"
